16e307a4e07f3e60756b57e42763ef255b93c20f
邊見　加愛 <>
2023-07-04 11:12:27 +0900

    ローディング追加

--- js/script.js
+++ js/script.js
@@ -23,6 +23,24 @@
 // });
 
 
+
+
+$(function () {
+    $(".overlay").show();
+    $.cookie('btnFlg') == 'on' ? $(".overlay").hide() : $(".overlay").show();
+    $(".btn_area button").click(function () {
+        $(".overlay").fadeOut();
+        $.cookie('btnFlg', 'on', { expires: 30, path: '/' }); //cookieの保存
+    });
+});
+
+
+
+
+
+
+
+
 $(window).on("scroll", function () {
     var windowTop = $(window).scrollTop();
     var windowBottom = windowTop + $(window).height();
@@ -30,22 +48,22 @@ $(window).on("scroll", function () {
     var elementBottom = elementTop + $(".kihon_area").height();
 
     if (elementTop <= windowTop && elementBottom >= windowTop) {
-        $(".kihon").addClass('menu_hover'); 
+        $(".kihon").addClass('menu_hover');
     } else {
-        $(".kihon").removeClass("menu_hover"); 
+        $(".kihon").removeClass("menu_hover");
     }
 });
 
 $(window).on("scroll", function () {
     var windowTop = $(window).scrollTop();
     var windowBottom = windowTop + $(window).height();
-    var elementTop = $(".nachu_area").offset().top-200;
+    var elementTop = $(".nachu_area").offset().top - 200;
     var elementBottom = elementTop + $(".nachu_area").height();
 
     if (elementTop <= windowTop && elementBottom >= windowTop) {
-        $(".nachu").addClass('menu_hover'); 
+        $(".nachu").addClass('menu_hover');
     } else {
-        $(".nachu").removeClass("menu_hover"); 
+        $(".nachu").removeClass("menu_hover");
     }
 });
 
@@ -53,13 +71,13 @@ $(window).on("scroll", function () {
 $(window).on("scroll", function () {
     var windowTop = $(window).scrollTop();
     var windowBottom = windowTop + $(window).height();
-    var elementTop = $(".hade_area").offset().top-300;
+    var elementTop = $(".hade_area").offset().top - 300;
     var elementBottom = elementTop + $(".hade_area").height();
 
     if (elementTop <= windowTop && elementBottom >= windowTop) {
-        $(".hade").addClass('menu_hover'); 
+        $(".hade").addClass('menu_hover');
     } else {
-        $(".hade").removeClass("menu_hover"); 
+        $(".hade").removeClass("menu_hover");
     }
 });
 
@@ -67,13 +85,13 @@ $(window).on("scroll", function () {
 $(window).on("scroll", function () {
     var windowTop = $(window).scrollTop();
     var windowBottom = windowTop + $(window).height();
-    var elementTop = $(".aimake_area").offset().top-300;
+    var elementTop = $(".aimake_area").offset().top - 300;
     var elementBottom = elementTop + $(".aimake_area").height();
 
     if (elementTop <= windowTop && elementBottom >= windowTop) {
-        $(".aimake").addClass('menu_hover'); 
+        $(".aimake").addClass('menu_hover');
     } else {
-        $(".aimake").removeClass("menu_hover"); 
+        $(".aimake").removeClass("menu_hover");
     }
 });
 
@@ -81,13 +99,13 @@ $(window).on("scroll", function () {
 $(window).on("scroll", function () {
     var windowTop = $(window).scrollTop();
     var windowBottom = windowTop + $(window).height();
-    var elementTop = $(".nayami_area").offset().top-300;
+    var elementTop = $(".nayami_area").offset().top - 300;
     var elementBottom = elementTop + $(".nayami_area").height();
 
     if (elementTop <= windowTop && elementBottom >= windowTop) {
-        $(".nayami").addClass('menu_hover'); 
+        $(".nayami").addClass('menu_hover');
     } else {
-        $(".nayami").removeClass("menu_hover"); 
+        $(".nayami").removeClass("menu_hover");
     }
 });
 
