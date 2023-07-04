3111d4d70c245d1ab212ceb8f2b88b0787b839e1
邊見　加愛 <>
2023-07-04 10:39:20 +0900

    pcメニュー　色変わるやつ完成

--- js/script.js
+++ js/script.js
@@ -1,39 +1,97 @@
 
-$(window).scroll(function () {
-    $(".kihon_area").each(function () {
-        var hit = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var wHeight = $(window).height();
+// $(window).scroll(function () {
+//     $(".kihon_area").each(function () {
+//         var hit = $(this).offset().top;
+//         var scroll = $(window).scrollTop();
+//         var wHeight = $(window).height();
 
-        if (scroll > hit - wHeight + wHeight / 100) {
-            $(".menu_item").addClass("menu_hover");
-        }
-    });
+//         if (scroll > hit - wHeight + wHeight / 100) {
+//             $(".menu_item").addClass("menu_hover");
+//         }
+//     });
+// });
+
+// $(window).scroll(function () {
+//     var top = $(".kihon_area").offset().top;
+//     var position = top - $(window).height();
+//     if ($(window).scrollTop() > position) {
+//         $(".kihon").addClass('menu_hover');
+//     }
+//     else {
+//         $(".kihon").removeClass("menu_hover");
+//     }
+// });
+
+
+$(window).on("scroll", function () {
+    var windowTop = $(window).scrollTop();
+    var windowBottom = windowTop + $(window).height();
+    var elementTop = $(".kihon_area").offset().top;
+    var elementBottom = elementTop + $(".kihon_area").height();
+
+    if (elementTop <= windowTop && elementBottom >= windowTop) {
+        $(".kihon").addClass('menu_hover'); 
+    } else {
+        $(".kihon").removeClass("menu_hover"); 
+    }
 });
 
-$(window).scroll(function () {
-    var top = $(".kihon_area").offset().top;
-    var position = top - $(window).height();
-    let navLink = document.getElementsByClassName('menu_item');
-    if ($(window).scrollTop() > position) {
-        console.log("aa");
-        $(".menu_item").addClass('menu_hover');
-        // $(".menu_item").each(function(){
-        //     if($(this).offset().top > $(window).scrollTop()) {
-        //         $(this).addClass('menu_hover');
-        //     }
-        // })
+$(window).on("scroll", function () {
+    var windowTop = $(window).scrollTop();
+    var windowBottom = windowTop + $(window).height();
+    var elementTop = $(".nachu_area").offset().top-200;
+    var elementBottom = elementTop + $(".nachu_area").height();
+
+    if (elementTop <= windowTop && elementBottom >= windowTop) {
+        $(".nachu").addClass('menu_hover'); 
+    } else {
+        $(".nachu").removeClass("menu_hover"); 
     }
-    else {
+});
+
+
+$(window).on("scroll", function () {
+    var windowTop = $(window).scrollTop();
+    var windowBottom = windowTop + $(window).height();
+    var elementTop = $(".hade_area").offset().top-300;
+    var elementBottom = elementTop + $(".hade_area").height();
+
+    if (elementTop <= windowTop && elementBottom >= windowTop) {
+        $(".hade").addClass('menu_hover'); 
+    } else {
+        $(".hade").removeClass("menu_hover"); 
+    }
+});
+
+
+$(window).on("scroll", function () {
+    var windowTop = $(window).scrollTop();
+    var windowBottom = windowTop + $(window).height();
+    var elementTop = $(".aimake_area").offset().top-300;
+    var elementBottom = elementTop + $(".aimake_area").height();
+
+    if (elementTop <= windowTop && elementBottom >= windowTop) {
+        $(".aimake").addClass('menu_hover'); 
+    } else {
+        $(".aimake").removeClass("menu_hover"); 
+    }
+});
+
+
+$(window).on("scroll", function () {
+    var windowTop = $(window).scrollTop();
+    var windowBottom = windowTop + $(window).height();
+    var elementTop = $(".nayami_area").offset().top-300;
+    var elementBottom = elementTop + $(".nayami_area").height();
+
+    if (elementTop <= windowTop && elementBottom >= windowTop) {
+        $(".nayami").addClass('menu_hover'); 
+    } else {
+        $(".nayami").removeClass("menu_hover"); 
     }
 });
 
 
 
 
-let navLink = document.getElementsByClassName('menu_item');
-for (let i = 0; i < navLink.length; i++) {
-    if (navLink[i].href === location.href) {
-        navLink[i].classList.add('menu_hover');
-    }
-}
+
