eaf45f3c80fd58b2ee5a944bbed8c82895aa31ee
邊見　加愛 <>
2023-07-18 11:18:32 +0900

    ファビコン追加

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
 
-// $(window).scroll(function () {
-//     $(".kihon_area").each(function () {
-//         var hit = $(this).offset().top;
-//         var scroll = $(window).scrollTop();
-//         var wHeight = $(window).height();
-
-//         if (scroll > hit - wHeight + wHeight / 100) {
-//             $(".menu_item").addClass("menu_hover");
-//         }
-//     });
-// });
-
-// $(window).scroll(function () {
-//     var top = $(".kihon_area").offset().top;
-//     var position = top - $(window).height();
-//     if ($(window).scrollTop() > position) {
-//         $(".kihon").addClass('menu_hover');
-//     }
-//     else {
-//         $(".kihon").removeClass("menu_hover");
-//     }
-// });
-
-
-
 
 $(function () {
     $(".overlay").show();
